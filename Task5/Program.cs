fe9c292ace5723525a39c20f02087d45ad21a453
kostaborozna <kostaborozna@gmail.com>
2022-12-15 05:25:41 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада");
+            int sum = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < 12; i++)
+            {
+                sum = Convert.ToInt32(sum + 0.7);
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
