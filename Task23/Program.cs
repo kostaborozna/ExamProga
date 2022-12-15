6b67844aee5b560dcf4dd6cf2b5231d928e5eb9e
kostaborozna <kostaborozna@gmail.com>
2022-12-15 05:44:00 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Задача23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводите элементы строкового массива, для остановки введите exit");
+            int i = 0;
+            string[] numbers = new string[1];
+            bool info = true;
+            while (info)
+            {
+                string input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    info = false;
+                }
+                else
+                {
+                    Array.Resize(ref numbers, 1 + i);
+                    numbers[i] = input;
+                    i++;
+                }
+            }
+            for(int j = 0; j < numbers.Length; j++)
+            {
+                Console.Write($"{numbers[j]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
