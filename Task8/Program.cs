65d78a89594014684bfc90712b51c8e135ebbd6e
kostaborozna <kostaborozna@gmail.com>
2022-12-15 05:34:32 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PathToFile меняем
+            string line = Console.ReadLine();
+            double path1;
+            double path2;
+            string PathToFile = @"C:\\Users\admin\source\repos\Task8\anser.txt";
+            if (line.Contains('+'))
+            {
+                path1 = Convert.ToInt32(line.Substring(0, line.IndexOf('+')));
+                path2 = Convert.ToInt32(line.Substring(line.IndexOf('+') + 1));
+                Console.WriteLine(path1 + path2);
+                CopyToFile(PathToFile, path1, path2, '+');
+
+            }
+            if (line.Contains('-'))
+            {
+                path1 = Convert.ToInt32(line.Substring(0, line.IndexOf('-')));
+                path2 = Convert.ToInt32(line.Substring(line.IndexOf('-') + 1));
+                Console.WriteLine(path1 - path2);
+                CopyToFile(PathToFile, path1, path2, '-');
+            }
+
+            if (line.Contains('*'))
+            {
+                path1 = Convert.ToInt32(line.Substring(0, line.IndexOf('*')));
+                path2 = Convert.ToInt32(line.Substring(line.IndexOf('*') + 1));
+                Console.WriteLine(path1 * path2);
+                CopyToFile(PathToFile, path1, path2, '-');
+            }
+
+            if (line.Contains('/'))
+            {
+                path1 = Convert.ToDouble(line.Substring(0, line.IndexOf('/')));
+                path2 = Convert.ToDouble(line.Substring(line.IndexOf('/') + 1));
+                if (path2 == 0)
+                {
+                    Console.WriteLine("На нолик делить нема");
+                }
+                else
+                {
+                    Console.WriteLine(path1 / path2);
+                    CopyToFile(PathToFile, path1, path2, '/');
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+        static string CopyToFile(string PathToFile, double path1, double path2, char sing)
+        {
+            
+            using (StreamWriter writer = new StreamWriter(PathToFile, true))
+            {
+                writer.WriteLine($"{path1} {sing} {path2}" + '\n');
+            }
+            return "";
+        }
+    }
+}
