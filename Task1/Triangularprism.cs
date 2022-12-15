b3aa70a99a6e45549d0a4a66815cdf43b1d08b98
kostaborozna <115942203+kostaborozna@users.noreply.github.com>
2022-12-15 05:28:54 +0300

    Task1

---
+++ Task1/Triangularprism.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp37
+{
+    class Triangularprism : Triangle
+    {
+        private int _height;
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public double AreaTrianglePrism()
+        {
+            return PerimeterCalculation() * Height + 2 * AreaTriangle();
+        }
+    }
+}
